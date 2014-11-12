a6bdaeba4c5951bcd2aca53f6edf2043e98c9309
Travis Harrison <guumby@gmail.com>
2014-11-12 15:43:00 -0600

    client script stub

--- plbin/njs_cli.pl
+++ plbin/njs_cli.pl
@@ -6,9 +6,5 @@ use warnings;
 require Bio::KBase::NarrativeJobService::Client;
 
 my $njs_obj = new Bio::KBase::NarrativeJobService::Client();
-my $status = $njs_obj->hello() || 'undefined';
-
-print "hello ".$status."\n";
-
 
 

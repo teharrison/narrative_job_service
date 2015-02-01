9b6879759792cd7aa70fbdee8d2bcf426892d4ee
non <chenry@mcs.anl.gov>
2015-02-01 02:24:00 -0600

    Changes to support community modeling workflow

--- scripts/njs-run-fba-modeling.pl
+++ scripts/njs-run-fba-modeling.pl
@@ -9,7 +9,7 @@ use strict;
 use warnings;
 use JSON::XS;
 use Getopt::Long;
-use Bio::KBase::workspace::ScriptHelpers qw(workspaceURL);
+use Bio::KBase::workspace::ScriptHelpers qw(workspaceURL get_ws_client);
 use Bio::KBase::fbaModelServices::ScriptHelpers qw(get_workspace_object fbaws printJobData get_fba_client runFBACommand universalFBAScriptCode );
 
 #Defining globals describing behavior
@@ -56,6 +56,7 @@ my $fba = get_fba_client($service_url);
 #Running command
 my $finalparameters = {};
 my $genomeset;
+my $modelset;
 foreach my $key (keys(%{$parameters})) {
 	if (length($parameters->{$key}) > 0) {
 		my $array = [split(/:/,$key)];
@@ -69,6 +70,9 @@ foreach my $key (keys(%{$parameters})) {
 			} else {
 				$current->{$array->[$i]} = $parameters->{$key};
 			}
+		}
+		if ($key eq "community_model_set") {
+			($modelset,my $info) = get_workspace_object($parameters->{workspace}."/".$finalparameters->{$key});
 		}
 		if ($key eq "community_submodel_ids") {
 			$finalparameters->{models} = [];
@@ -86,6 +90,35 @@ foreach my $key (keys(%{$parameters})) {
 		if ($key eq "pangenome_set") {
 			($genomeset,my $info) = get_workspace_object($parameters->{workspace}."/".$finalparameters->{$key});
 		}
+	}
+}
+if (defined($modelset) && $command eq "models_to_community_model") {
+	my $hash;
+	my $count = 0;
+	if (defined($finalparameters->{models})) {
+		for (my $i=0; $i < @{$finalparameters->{models}}; $i++) {
+			if (!defined($hash->{$finalparameters->{models}->[$i]->[1]."/".$finalparameters->{models}->[$i]->[0]})) {
+				$hash->{$finalparameters->{models}->[$i]->[1]."/".$finalparameters->{models}->[$i]->[0]} = $finalparameters->{models}->[$i]->[2];
+				$count++;
+			}
+		}
+	}
+	foreach my $key (keys(%{$modelset->{"elements"}})) {
+		my $ref = $modelset->{"elements"}->{$key}->{"ref"};
+		my $array = [split(/\//,$ref)];
+		my $abundance = 1;
+		if (defined($modelset->{"elements"}->{$key}->{metadata}->{abundance})) {
+			$abundance = $modelset->{"elements"}->{$key}->{metadata}->{abundance};
+		}
+		if (!defined($hash->{$array->[0]."/".$array->[1]})) {
+			$hash->{$array->[0]."/".$array->[1]} = $abundance;
+			$count++;
+		}
+	}
+	$finalparameters->{models} = [];
+	foreach my $item (keys(%{$hash})) {
+		my $array = [split(/\//,$item)];
+		push(@{$finalparameters->{models}},[$array->[1],$array->[0],$hash->{$item}]);
 	}
 }
 if (defined($genomeset)) {
@@ -124,6 +157,49 @@ if ($command eq "gapfill_model" && !defined($finalparameters->{formulation}->{fo
 	$finalparameters->{formulation}->{formulation}->{media_workspace} = "KBaseMedia";
 }
 my $JSON = JSON->new->utf8(1);
-print STDOUT $JSON->encode($finalparameters);
 my $output = $fba->$command($finalparameters);
+if ($command eq "metagenome_to_fbamodels") {
+	my $object = {
+		type => "KBaseFBA.FBAModelSet",
+		data => {
+			description => "Set of models generated from metagenome ".$finalparameters->{workspace}."/".$finalparameters->{metaanno_uid},
+			elements => {}
+		},
+		provenance => [{
+			"time" => DateTime->now()->datetime()."+0000",
+			service_ver => $fba->version(),
+			service => "KBaseFBAModeling",
+			method => $command,
+			method_params => [$parameters],
+			input_ws_objects => [],
+			resolved_ws_objects => [],
+			intermediate_incoming => [],
+			intermediate_outgoing => []
+		}],
+	};
+	for (my $i=0; $i < @{$output}; $i++) {
+		$object->{data}->{elements}->{$output->[$i]->[7]."/".$output->[$i]->[1]} = {
+			"ref" => $output->[$i]->[6]."/".$output->[$i]->[0]."/".$output->[$i]->[4],
+			metadata => {
+				otu => $output->[$i]->[10]->{Name},
+				numreactions => $output->[$i]->[10]->{"Number reactions"}	
+			}
+		};
+	}
+	if ($finalparameters->{modelset_id} =~ m/^\d+$/) {
+		$object->{objid} = $finalparameters->{modelset_id};
+	} else {
+		$object->{name} = $finalparameters->{modelset_id};
+	}	
+	my $input = {
+		objects => [$object], 	
+	};
+	if ($finalparameters->{workspace}  =~ m/^\d+$/) {
+	   	$input->{id} = $finalparameters->{workspace};
+	} else {
+	   	$input->{workspace} = $finalparameters->{workspace};
+	}
+	my $ws = get_ws_client($ws_url);
+	$output = $ws->save_objects($input);
+}
 print STDOUT $JSON->encode($output);
